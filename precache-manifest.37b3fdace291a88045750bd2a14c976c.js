1e48528dc2a6352aac92f0d0226ef6cc09136ebe
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-06-17 17:17:42 +0100

    Updates

---
+++ precache-manifest.37b3fdace291a88045750bd2a14c976c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c4182ef79f72a109318814a84bcbce",
+    "url": "/react-milestone-2/index.html"
+  },
+  {
+    "revision": "1cf91bfdb380293a406b",
+    "url": "/react-milestone-2/static/css/main.cf09787a.chunk.css"
+  },
+  {
+    "revision": "24d6499a0c2c800b9a9e",
+    "url": "/react-milestone-2/static/js/2.b8c30602.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-2/static/js/2.b8c30602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf91bfdb380293a406b",
+    "url": "/react-milestone-2/static/js/main.1fef9580.chunk.js"
+  },
+  {
+    "revision": "44a38e3d01dfdee1500c",
+    "url": "/react-milestone-2/static/js/runtime-main.fba5330c.js"
+  }
+]);
